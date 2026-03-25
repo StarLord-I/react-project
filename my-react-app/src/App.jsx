8ff656a4c427360da502a49d2f36ef348b5d55b3
jiya khan pathan <jiyakhanpathan45@gmail.com>
2026-03-25 20:41:43 +0530

    added components and updated UI, remove unused assets

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,17 @@
+import Card from "./components/card";
+import Footer from "./components/footer";
+import Header from "./components/header";
+
 function App(){
-  return 'hello my work done successfully'
+  return (
+    <div>
+      <Header />
+      <Card />
+      <h1>hello my work done successfully</h1>
+      
+      <Footer />
+      
+    </div>
+  );
 }
 export default App;
---
+++ my-react-app/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './card.css'
+function Card() {
+  return (
+    <div className='card'>
+        <h3>Jiya Khan Pathan </h3>
+        <p>learning React and making apps and websites for fun!</p>
+    </div>
+  )
+}
+
+export default Card
