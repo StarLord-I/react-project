a2143cf4cea5711dde6b48198c71366fe880f6f4
jiya khan pathan <jiyakhanpathan45@gmail.com>
2026-03-27 23:14:28 +0530

    updated card UI and assets

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,8 @@
-import Card from './components/card'
-import pic from './asset/images/pic.jpg'
+import Card from "./components/card"; 
 function App(){
   return (
     <div>
-       <Card title='jiya khan pathan' desc='learning React and making apps and websites for fun!' imgSrc={pic} />
+        <Card/>
        
     </div>
   );
--- my-react-app/src/components/card.jsx
+++ my-react-app/src/components/card.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Bookmark } from 'lucide-react';
+import amazonlogo from '../assets/amazon.png'
 import './card.css'
-function Card(props) {
+function Card() {
   return (
-    <div className='card'>
-        <img src={props.imgSrc} alt="profile" className='profile-img'/>
-       <h3 className='name'>{props.title}</h3>
-       <p className='bio'>{props.desc}</p>
+    <div className="parent">
+
+      <div className="card">
+
+         <div className="top">
+        <img src={amazonlogo} alt="logo" />
+        <button>save <Bookmark size={12} /></button>
+         </div>
+         
+         <div className="center">
+          <h3>Amazon<span>5 days ago</span></h3>
+          <h2>Senior UI/UX Designer</h2>
+          <div>
+            <h4>part time</h4>
+            <h4>Senior level</h4>
+          </div>
+
+         </div>
+
+
+
+         <div className="bottom"> 
+          <div>
+            <h3>120$/hour</h3>
+            <p>mumbai,India</p>
+            <button>apply now</button>
+          </div>
+         </div>
+
+      </div>
     </div>
   )
 }
